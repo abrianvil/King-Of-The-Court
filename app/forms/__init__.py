5b80cb4c1cb6c43d8e5b34833f13b0f57eac9ddd
abel <abrianvil@gmail.com>
2023-03-02 14:20:45 -0500

    untracked

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,2 +1,5 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
+from .court_form import New_Court
+from .team_form import New_team
+from .game_form import New_game
--- app/forms/court_form.py
+++ app/forms/court_form.py
@@ -3,6 +3,6 @@ from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired
 
 
-class new_Court (FlaskForm):
+class New_Court (FlaskForm):
     num_of_hoops = IntegerField('num_of_hoops', validators=[DataRequired()])
     surface_type = StringField('surface_type')
